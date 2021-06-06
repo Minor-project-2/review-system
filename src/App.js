cfeed56eaf33a56bb3859a981609b323fb2f05b3
Anurag114211817 <anuragchakraborty735@gmail.com>
2021-06-06 23:25:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   // Switch,
+//   // Route,
+//   // Link
+// } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+			<div className="slider fullscreen">
+				<ul className="slides">
+					<li> <img src="images/slider/1.jpg" alt="" />
+					
+						<div className="caption center-align slid-cap">
+							<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+							<h2>This is our big Tagline!</h2>
+							<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <button className="btn btn-sm btn-danger waves-effect waves-light m-5">Booking</button> <button className="btn btn-sm btn-danger waves-effect waves-light">Booking</button> </div>
+					</li>
+					<li> <img src="images/slider/2.jpg" alt="" />
+					
+						<div className="caption center-align slid-cap">
+							<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+							<h2>This is our big Tagline!</h2>
+							<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <button className="btn btn-sm btn-danger waves-effect waves-light m-5">Booking</button> <button className="btn btn-sm btn-danger waves-effect waves-light">Booking</button> </div>
+					</li>
+					<li> <img src="images/slider/3.jpg" alt="" />
+						
+						<div className="caption center-align slid-cap">
+							<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+							<h2>This is our big Tagline!</h2>
+							<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <button className="btn btn-sm btn-danger waves-effect waves-light m-5">Booking</button> <button className="btn btn-sm btn-danger waves-effect waves-light">Booking</button> </div>
+					</li>
+					<li> <img src="images/slider/4.jpg" alt="" />
+						
+						<div className="caption center-align slid-cap">
+							<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+							<h2>This is our big Tagline!</h2>
+							<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <button className="btn btn-sm btn-danger waves-effect waves-light m-5">Booking</button> <button className="btn btn-sm btn-danger waves-effect waves-light" >Booking</button> </div>
+					</li>
+				</ul>
+			</div>
+		</div>
+    </>
   );
 }
 
