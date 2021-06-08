486e0e1f7fcffd285741ff41941a18a776d30bbc
Anurag114211817 <anuragchakraborty735@gmail.com>
2021-06-08 12:58:01 +0530

    3rd commit

--- src/MyComponents/Banner.js
+++ src/MyComponents/Banner.js
@@ -1,42 +1,58 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export const Banner = () => {
     return (
         <>
             <section>
-            <div className="slider fullscreen">
-				<ul className="slides">
-					<li> 
-						<img src="images/slider/1.jpg" alt="" />
-						<div className="caption center-align slid-cap">
-							<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
-							<h2>This is our big Tagline!</h2>
-							<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <button className="btn btn-sm btn-danger waves-effect waves-light m-5">Booking</button> <button className="btn btn-sm btn-danger waves-effect waves-light">Booking</button> 
-						</div>
-					</li>
-					<li> 
-						<img src="images/slider/2.jpg" alt="" />
-						<div className="caption center-align slid-cap">
-							<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
-							<h2>This is our big Tagline!</h2>
-							<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <button className="btn btn-sm btn-danger waves-effect waves-light m-5">Booking</button> <button className="btn btn-sm btn-danger waves-effect waves-light">Booking</button> </div>
-					</li>
-					<li> 
-						<img src="images/slider/3.jpg" alt="" />
-						<div className="caption center-align slid-cap">
-							<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
-							<h2>This is our big Tagline!</h2>
-							<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <button className="btn btn-sm btn-danger waves-effect waves-light m-5">Booking</button> <button className="btn btn-sm btn-danger waves-effect waves-light">Booking</button> </div>
-					</li>
-					<li> 
-						<img src="images/slider/4.jpg" alt="" />
-						<div className="caption center-align slid-cap">
-							<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
-							<h2>This is our big Tagline!</h2>
-							<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <button className="btn btn-sm btn-danger waves-effect waves-light m-5">Booking</button> <button className="btn btn-sm btn-danger waves-effect waves-light" >Booking</button> </div>
-					</li>
-				</ul>
-			</div>
+				<div className="slider fullscreen">
+					<ul className="slides">
+						<li> 
+							<img src="images/slider/1.jpg" alt="" />
+							<div className="caption center-align slid-cap">
+								<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+								<h2>This is our big Tagline!</h2>
+								<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.
+								   Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, 
+								   pellentesque id mi.</p> 
+								<Link to="#!" className="waves-effect waves-light">Booking</Link> 
+								<Link to="#!" className="waves-effect waves-light">Booking</Link> 
+							</div>
+						</li>
+						<li> 
+							<img src="images/slider/2.jpg" alt="" />
+							<div className="caption center-align slid-cap">
+								<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+								<h2>This is our big Tagline!</h2>
+								<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.
+								   Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, 
+								   pellentesque id mi.</p> 
+								<Link to="#!" className="waves-effect waves-light">Booking</Link> 
+								<Link to="#!" className="waves-effect waves-light">Booking</Link> 
+							</div>
+						</li>
+						<li>  
+							<img src="images/slider/3.jpg" alt="" />
+							<div className="caption center-align slid-cap">
+								<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+								<h2>This is our big Tagline!</h2>
+								<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.
+								   Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, 
+								   pellentesque id mi.</p> 
+								<Link to="#!" className="waves-effect waves-light">Booking</Link> 
+								<Link to="#!" className="waves-effect waves-light">Booking</Link> 
+							</div>
+						</li>
+						<li> 
+							<img src="images/slider/4.jpg" alt="" />
+							<div className="caption center-align slid-cap">
+								<h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+								<h2>This is our big Tagline!</h2>
+								<p>Mauris non placerat nulla. Sed vestibulum quam mauris, et malesuada tortor venenatis at.Aenean euismod sem porta est consectetur posuere. Praesent nisi velit, porttitor ut imperdiet a, pellentesque id mi.</p> <Link className="waves-effect waves-light">Booking</Link> <Link className="waves-effect waves-light" >Booking</Link> 
+							</div>
+						</li>
+					</ul>
+				</div>
             </section>
         </>
     )
