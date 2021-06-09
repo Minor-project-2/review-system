17c45afe64a950152fb9efd3245c6d5db99d892d
Gaurav Patel <34380366+gaurav-patel-git@users.noreply.github.com>
2021-06-09 20:50:02 +0530

    Create FoodMenu.jsx

---
+++ src/MyComponents/FoodMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FoodMenu() {
+    return (
+       
+            <div id="home" class="tab-pane fade tab-space">
+                <div class="res-menu"> <img src="images/menu/1.jpg" alt="" />
+                    <h3>salted fried chicken <span>$45</span></h3> <span class="menu-item">Tomato soup with croutons, small ceasar salad, apple juice</span> </div>
+            </div>
+       
+    )
+}
+
+export default FoodMenu
