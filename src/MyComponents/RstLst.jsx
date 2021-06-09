641dffa0e7d0ed24942792b603e0ed3c0980faf8
Gaurav Patel <34380366+gaurav-patel-git@users.noreply.github.com>
2021-06-09 20:52:19 +0530

    Create RstLst.jsx

---
+++ src/MyComponents/RstLst.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import RstLstItem from "./RstLstItem"
+function RstLst() {
+    return (
+        <div>
+           
+			<div className="container">
+				<div className="row">
+					<div className="page-head">
+						<h2>Room Types</h2>
+						<div className="head-title">
+							<div className="hl-1"></div>
+							<div className="hl-2"></div>
+							<div className="hl-3"></div>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+					</div>
+					<RstLstItem />
+                   
+				</div>
+			</div>
+		
+</div>
+    )
+}
+
+export default RstLst
